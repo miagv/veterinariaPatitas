c782751f74a867ca2bc103c42f24b6461ade4c35
ju4n0ff <xlol553@gmail.com>
2025-11-04 20:39:37 -0500

    clean

--- veterinariaPatitas-f96a2476d50b8b273843f58c74ec900bf524999b/src/main/java/com/example/veterinariaPatitas/repository/HorarioMedicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.veterinariaPatitas.repository;
-
-import com.example.veterinariaPatitas.model.HorarioMedico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HorarioMedicoRepository extends JpaRepository<HorarioMedico, Long> {
-}
